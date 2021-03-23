cc069250505478308002f83e855b3b65a40094cb
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2021-03-23 21:05:35 +0530

    Contact Book - Fixed styling of navbar

--- contact-book/src/components/Header.js
+++ contact-book/src/components/Header.js
@@ -1,31 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 
-export default class Header extends React.Component {
-  render() {
-    return (
-      <nav className="navbar navbar-expand-sm sticky-top navbar-light mb-4">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            <i className="fa fa-book" aria-hidden="true"></i>
-            {this.props.branding}
-          </a>
+const Header = (props) => {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+  const navCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
-          <div>
-            <ul className="navbar-nav nav-pills ml-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Home
-                </a>
-              </li>
-            </ul>
-          </div>
+  return (
+    <nav className="navbar navbar-expand-sm sticky-top mb-4">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          <i className="fa fa-book" aria-hidden="true"></i>
+          {props.branding}
+        </a>
+
+        <button
+          className="navbar-toggler navbar-light"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarMobile"
+          style={{ border: "1px solid black" }}
+          onClick={navCollapse}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`}
+          id="navbarMobile"
+        >
+          <ul className="navbar-nav nav-pills ml-auto">
+            <li className="nav-item">
+              <a className="nav-link pr-1 text-right" href="/">
+                Home
+              </a>
+            </li>
+          </ul>
         </div>
-      </nav>
-    );
-  }
-}
+      </div>
+    </nav>
+  );
+};
 
 Header.defaultProps = {
   branding: " Contact Book",
 };
+
+export default Header;
