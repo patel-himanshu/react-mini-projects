310f355bb5fa745dc562cc277a003d8de7a50504
Himanshu Patel <44549418+patel-himanshu@users.noreply.github.com>
2021-03-28 21:59:11 +0530

    style: changed website layout

--- src/layouts/HomePage.js
+++ src/layouts/HomePage.js
@@ -5,7 +5,7 @@ export default class HomePage extends Component {
   render() {
     return (
       <div className="container mt-4">
-        <h1 className="mb-2">These are React Mini-Projects</h1>
+        <h1 className="mb-4">React Mini Projects :</h1>
         <ul className="list-group">
           <li className="list-group-item" style={{ fontSize: "1.5rem" }}>
             <Link to="contact-book/">Contact Book</Link>
--- src/layouts/Page404.js
+++ src/layouts/Page404.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Page404 extends Component {
   render() {
     return (
-      <div>
+      <div className="container mt-4">
         <h1
           className="font-weight-bold text-danger"
-          style={{ fontSize: "2rem" }}
+          style={{ fontSize: "2.25rem" }}
         >
           404: Page Not Found
         </h1>
-        <p>Sorry, the page you are looking for doesn't exist.</p>
+        <p>
+          Sorry, the page you are looking for doesn't exist.
+          <br />
+          Go back to <Link to="/">Home</Link>.
+        </p>
       </div>
     );
   }
